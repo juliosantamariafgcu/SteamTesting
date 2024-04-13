7382d9a4510028f703f56c386d11382fd0211468
Daniel Kareh <33532334+dkareh@users.noreply.github.com>
2024-04-13 12:59:32 -0400

    Add helper classes for testing Steam
    `WebDriverTest` creates and quits a web driver instance. The environment
    variable `STEAM_WEB_DRIVER` chooses which browser to use. Its value must
    be 'Chrome', 'Edge', or 'Firefox'. If the environment variable doesn't
    exist, the default choice is Chrome.
    
    `AuthenticatedSteamTest` signs in and out of Steam. To avoid committing
    Steam credentials, the account name and password must be passed through
    the environment using `STEAM_ACCOUNT_NAME` and `STEAM_PASSWORD`.
    
    Both classes are meant to be extended by test classes.

---
+++ src/main/java/testing/steam/AuthenticatedSteamTest.java
@@ -0,0 +1,63 @@
+package testing.steam;
+
+import java.time.Duration;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+public class AuthenticatedSteamTest extends WebDriverTest {
+    private final String ACCOUNT_NAME = "STEAM_ACCOUNT_NAME";
+    private final String PASSWORD = "STEAM_PASSWORD";
+    private final Duration SIGN_IN_TIMEOUT = Duration.ofSeconds(5);
+
+    @BeforeClass
+    public void signIn() throws InterruptedException {
+        driver.get("https://store.steampowered.com/login/");
+
+        String accountName = System.getenv(ACCOUNT_NAME);
+        String password = System.getenv(PASSWORD);
+
+        Wait<WebDriver> wait = new WebDriverWait(driver, SIGN_IN_TIMEOUT);
+        By accountNameTextBoxLocator = By.cssSelector("div.page_content form input[type='text']");
+        By passwordTextBoxLocator = By.cssSelector("div.page_content form input[type='password']");
+        By signInButtonLocator = By.cssSelector("div.page_content form button[type='submit']");
+        wait.until(
+            ExpectedConditions.and(
+                ExpectedConditions.visibilityOfElementLocated(accountNameTextBoxLocator),
+                ExpectedConditions.visibilityOfElementLocated(passwordTextBoxLocator),
+                ExpectedConditions.elementToBeClickable(signInButtonLocator)
+            )
+        );
+
+        // NOTE(Daniel): Small delays make the sign in process succeed more consistently.
+        WebElement accountNameTextBox = driver.findElement(accountNameTextBoxLocator);
+        WebElement passwordTextBox = driver.findElement(passwordTextBoxLocator);
+        WebElement signInButton = driver.findElement(signInButtonLocator);
+        accountNameTextBox.sendKeys(accountName);
+        Thread.sleep(500);
+        passwordTextBox.sendKeys(password);
+        Thread.sleep(500);
+        signInButton.click();
+
+        // Wait until the sign in process is complete.
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("store_header")));
+        Assert.assertEquals(driver.getCurrentUrl(), "https://store.steampowered.com/");
+    }
+
+    @AfterClass
+    public void signOut() {
+        driver.get("https://store.steampowered.com/");
+
+        WebElement pulldown = driver.findElement(By.id("account_pulldown"));
+        pulldown.click();
+
+        WebElement dropdown = driver.findElement(By.id("account_dropdown"));
+        WebElement signOutLink = dropdown.findElement(By.xpath(".//a[@class='popup_menu_item'][last()]"));
+        signOutLink.click();
+    }
+};
---
+++ src/main/java/testing/steam/WebDriverTest.java
@@ -0,0 +1,48 @@
+package testing.steam;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeDriverService;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.GeckoDriverService;
+import org.testng.annotations.*;
+
+public class WebDriverTest {
+    private final String WEB_DRIVER = "STEAM_WEB_DRIVER";
+
+    protected WebDriver driver;
+
+    @BeforeTest
+    public void setDefaultDriverLocations() {
+        // System.setProperty(ChromeDriverService.CHROME_DRIVER_EXE_PROPERTY, "");
+        // System.setProperty(EdgeDriverService.EDGE_DRIVER_EXE_PROPERTY, "");
+        // System.setProperty(GeckoDriverService.GECKO_DRIVER_EXE_PROPERTY, "");
+    }
+
+    @BeforeClass
+    public void createDriver() {
+        String choice = System.getenv().getOrDefault(WEB_DRIVER, "Chrome");
+        switch (choice) {
+            case "Chrome":
+                driver = new ChromeDriver();
+                break;
+            case "Edge":
+                driver = new EdgeDriver();
+                break;
+            case "Firefox":
+                driver = new FirefoxDriver();
+                break;
+            default:
+                String message = WEB_DRIVER + " must be 'Chrome', 'Edge', or 'Firefox'; "
+                    + "currently it is '" + choice + "'";
+                throw new RuntimeException(message);
+        }
+    }
+
+    @AfterClass(alwaysRun = true)
+    public void quitDriver() {
+        driver.quit();
+    }
+};

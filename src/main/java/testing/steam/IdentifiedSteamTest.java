af641ba0a38c08df21c9faf204ff1fc8281349fd
Daniel Kareh <33532334+dkareh@users.noreply.github.com>
2024-04-16 10:23:17 -0400

    Fetch test account's display name in `IdentifiedSteamTest`

--- src/main/java/testing/steam/IdentifiedSteamTest.java
+++ src/main/java/testing/steam/IdentifiedSteamTest.java
@@ -10,17 +10,26 @@ public class IdentifiedSteamTest extends AuthenticatedSteamTest {
     private static final String ACCOUNT_URL = "https://store.steampowered.com/account/";
 
     private String steamId;
+    private String displayName;
 
     @BeforeClass
     public void readSteamIdFromAccountPage() {
         driver.get(ACCOUNT_URL);
         WebElement steamIdElement = driver.findElement(By.className("youraccount_steamid"));
+        WebElement accountPulldown = driver.findElement(By.id("account_pulldown"));
         steamId = steamIdElement.getText().replace("Steam ID: ", "").trim();
+        displayName = accountPulldown.getText().trim();
         Assert.assertTrue(steamId.length() > 0);
+        Assert.assertTrue(displayName.length() > 0);
         System.out.format("Steam ID: %s\n", steamId);
+        System.out.format("Display Name: %s\n", displayName);
     }
 
     protected String getSteamId() {
         return steamId;
+    }
+
+    protected String getDisplayName() {
+        return displayName;
     }
 };
